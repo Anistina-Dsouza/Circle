0056342c3adbaa8b771e6d48d10ababf3ede3bb7
Maryam Shaikh <mshaikh191103@gmail.com>
2026-02-27 23:26:57 +0530

    "changes in routes"

--- frontend/src/features/admin/components/Sidebar.jsx
+++ frontend/src/features/admin/components/Sidebar.jsx
@@ -1,34 +1,3 @@
-// import { LayoutDashboard, Users, Megaphone, BarChart, LogOut } from "lucide-react";
-
-// export default function Sidebar() {
-//   return (
-//     <aside className="sidebar w-64 p-6 flex flex-col">
-
-//       <h2 className="text-2xl mb-10">Circle</h2>
-
-//       <nav className="space-y-4 flex-1">
-
-//         <Item icon={<LayoutDashboard/>} label="Dashboard" active/>
-//         <Item icon={<Users/>} label="Users"/>
-//         <Item icon={<Users/>} label="Communities"/>
-//         <Item icon={<Megaphone/>} label="Announcements"/>
-//         <Item icon={<BarChart/>} label="Reports"/>
-
-//       </nav>
-
-//       <Item icon={<LogOut/>} label="Logout"/>
-
-//     </aside>
-//   );
-// }
-
-// const Item = ({icon,label,active}) => (
-//   <div className={`flex items-center gap-3 px-4 py-3 rounded-full cursor-pointer
-//   ${active?"bg-purple-600":"hover:bg-purple-800"}`}>
-//     {icon}{label}
-//   </div>
-// );
-
 import { Link } from "react-router-dom";
 import { LayoutDashboard, Users, Layers, Megaphone, BarChart, LogOut } from "lucide-react";
 
@@ -53,17 +22,18 @@ export default function Sidebar() {
       <nav className="flex-1 space-y-4 overflow-y-auto pr-2">
 
         <Item icon={<LayoutDashboard size={18} />} label="Dashboard" active />
-<<<<<<< HEAD
+
         <Link to="/admin/users">
           <Item icon={<Users size={18} />} label="Users" />
         </Link>
+
         <Link to="/admin/communities">
           <Item icon={<Layers size={18} />} label="Communities" />
         </Link>
-=======
+
         <Item icon={<Users size={18} />} label="Users" />
         <Item icon={<Users size={18} />} label="Circles" />
->>>>>>> 91c860c3a7a05ff3a9f12247a0d7aad2364af9a2
+
         <Item icon={<Megaphone size={18} />} label="Announcements" />
         <Item icon={<BarChart size={18} />} label="Reports" />
 
